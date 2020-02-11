c2df3f2e574410fb86fd27b9250015ed5afe4c8d
GerardProject <gerardklee@hotmail.com>
2020-02-11 11:45:16 -0800

    socket.io implemented

--- brainwaves.js
+++ brainwaves.js
@@ -1,5 +1,19 @@
+// node version 7.8.0 required
+// npm install --save package-name
+// tone.js for sound feedback
+// charts.js for real time graphing
+// killall -9 node // to kill all the node processes
+// smoothing: bandpass filtering, threshold
+
 const { Wifi } = require('openbci-observable');
 const { voltsToMicrovolts, bufferFFT, powerByBand } = require('eeg-pipes');
+
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+io.on('connection', client => {
+  client.on('event', data => { });
+  client.on('disconnect', () => { });
+});
 
 async function init () {
     console.log("funtion in");
@@ -7,6 +21,7 @@ async function init () {
     await wifi.connect({ ipAddress: '192.168.4.1' });
     await wifi.start();
     console.log("streaming starts");
+
     wifi.stream.pipe(
         voltsToMicrovolts(),
         bufferFFT({ bins: 256 }),
@@ -14,19 +29,31 @@ async function init () {
     ).subscribe(bandPowers =>
         {
             // print all the channels
-            console.log(bandPowers["alpha"]);
+            //io.emit("alpha data", bandPowers["alpha"]);
             // get average of all the channels
+            //console.log("subscribe starts")
             var alpha = bandPowers["alpha"];
-            totalChannel = 8;
+            var delta = bandPowers["delta"];
+            console.log("alpha: ", alpha)
+            console.log("delta: ", delta)
+            //console.log("band power show");
+            totalChannels = 8;
             average = 0;
             total = 0;
             for (var i = 0; i < alpha.length; i++) {
                 total += alpha[i];
             }
-            average = total / totalChannel;     
-            console.log("average: " + average);      
+            //console.log("show average");
+            //average = total / totalChannels;     
+            //console.log(average);
+            //io.emit("average", average); 
+            //console.log("average shown");   
         }
     );
 }
 
-init();
+async function start() {
+    init();
+    server.listen(3000);
+}
+start();
