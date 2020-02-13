c8cadc61699c1336641c6720f297a2da25e08fcd
GerardProject <gerardklee@hotmail.com>
2020-02-13 14:32:43 -0800

    plot and output to csv. signals correspond to eye blinks

--- rawstream.js
+++ rawstream.js
@@ -1,6 +1,12 @@
+const Wifi = require('@openbci/wifi');
+const OpenBCIConsts = require("openbci-utilities").Constants;
+const createCsvWriter = require('csv-writer').createArrayCsvWriter;
+const csvWriter = createCsvWriter({
+  path: '/Users/gerardlee/Desktop/raw.csv',
+  header: ['FIRST CHANNEL']
+});
+
 function init() {
-    const Wifi = require('@openbci/wifi');
-    const OpenBCIConsts = require("openbci-utilities").Constants;
     let wifi = new Wifi({
         debug: false,
         verbose: true,
@@ -9,14 +15,20 @@ function init() {
 
     wifi.on('sample',(sample) => {
         try {
-          console.log("raw data: ", sample.channelData);
+          const firstChannel = [[-sample.channelData[0]]];
+          console.log("raw data: "); 
+          console.log(firstChannel);
+          csvWriter.writeRecords(firstChannel)
+            .then(() => {
+              console.log('recorded');
+            });
         } catch (err) {
           console.log(err);
         }
     })
 
     wifi.searchToStream({
-        sampleRate: 1000, // Custom sample rate
+        sampleRate: 250, // Custom sample rate
         shieldName: 'OpenBCI-9630', // Enter the unique name for your wifi shield
         streamStart: true // Call to start streaming in this function
       }).catch(console.log);
