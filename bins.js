041cc6d83bff328b4a0dfba9af302faefae3b6f8
GerardProject <gerardklee@hotmail.com>
2020-02-20 11:02:09 -0800

    graph added

--- bins.js
+++ bins.js
@@ -1,5 +1,5 @@
 const { Wifi } = require('openbci-observable');
-const { voltsToMicrovolts, bufferFFT, powerByBand } = require('eeg-pipes');
+const { voltsToMicrovolts, bufferFFT, powerByBand, sliceFFT, epoch, fft } = require('@neurosity/pipes');
 
 const server = require('http').createServer();
 const io = require('socket.io')(server);
@@ -7,6 +7,8 @@ io.on('connection', client => {
   client.on('event', data => { });
   client.on('disconnect', () => { });
 });
+
+const data1 = {"psd":[[27.197596442311976,23.479118879048993,19.781206817947567,15.643339824322986,12.638206260596553,11.081112933731703,8.730078767878478],[53.29126980547793,51.578601544362776,43.10380950455574,33.91020488217584,89.68125343001996,69.41522429915457,69.83147361181165],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]],"freqs":[7,8,9,10,11,12,13],"info":{"startTime":1582042659234,"samplingRate":256}};
 
 async function init () {
     console.log("funtion in");
@@ -22,17 +24,19 @@ async function init () {
     
     wifi.stream.pipe(
         voltsToMicrovolts(),
-        bufferFFT({ bins: 128, samplingRate: 256 })
-    ).subscribe(bins => 
+        epoch({ duration: 256, interval: 100, samplingRate: 256 }),
+        fft({ bins: 256}),
+        sliceFFT([7, 13])
+    ).subscribe(data => 
         {
-            console.log(bins[0].length);
-            console.log(bins[0][1]);
+            io.emit("data", data);
         }
     );
 }
 
 async function start() {
-    init();
+    //init();
     server.listen(3000);
+    setInterval(() => io.emit("data", data1), 1000);
 }
 start();
