0f6aa4547d783aa99d26f5fd2ee51bf15e140e33
GerardProject <gerardklee@hotmail.com>
2020-02-24 21:31:37 -0800

    frequency res. fixed and area of line removed

--- bins.js
+++ bins.js
@@ -10,7 +10,6 @@ io.on('connection', client => {
 const data1 = {"psd":[[27.197596442311976,23.479118879048993,19.781206817947567,15.643339824322986,12.638206260596553,11.081112933731703,8.730078767878478],[53.29126980547793,51.578601544362776,43.10380950455574,33.91020488217584,89.68125343001996,69.41522429915457,69.83147361181165],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]],"freqs":[7,8,9,10,11,12,13],"info":{"startTime":1582042659234,"samplingRate":256}};
 
 async function init () {
-    console.log("funtion in");
     const wifi = new Wifi();
     await wifi.connect({ ipAddress: '192.168.4.1' });
     await wifi.start();
@@ -25,7 +24,7 @@ async function init () {
         voltsToMicrovolts(),
         epoch({ duration: 256, interval: 100, samplingRate: 256 }),
         fft({ bins: 1024 }),
-        sliceFFT([7, 13])
+        sliceFFT([7, 13.75])
     ).subscribe(data => 
         {
             io.emit("data", data);
@@ -35,8 +34,8 @@ async function init () {
 }
 
 async function start() {
-    init();
+    //init();
     server.listen(3000);
-    //setInterval(() => io.emit('fft_data', data1), 1000);
+    setInterval(() => io.emit('fft_data', data1), 1000);
 }
 start();
